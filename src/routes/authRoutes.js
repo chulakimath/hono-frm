8b6d7e86788b7e9aa44fc418af7e9671a7094460
chulakimath <santoshchulakimath222@gmail.com>
2026-07-25 10:58:02 +0530

    jwt from hono

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,9 @@
 import { Hono } from 'hono';
-import * as AUthController from '../controllers/authController.js';
+import * as AuthController from '../controllers/authController.js';
 import { AuthCheck } from '../middleware/AuthCheck.js';
 
 const router = new Hono();
-router.post('/', AUthController.login);
-router.get('/me', AuthCheck, AUthController.me);
+router.post('/', AuthController.login);
+router.get('/me', AuthCheck, AuthController.me);
 
 export default router;
